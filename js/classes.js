1ab87224653ca1a460f14bc69767433361532ed2
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-04-11 22:14:13 +0200

    build the classes,some dom, some css

--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,84 @@
+/*my game needs:
+A player who has to catch the 4 elements (I will call them Necessities) to satisfy the baby in a short timeframe: milk, diaper, toy, nap time. 
+Otherwise baby cries and you loose
+I wanted to have a nanny or mommy run right left to catch all the necessities. 
+Other option maybe more simple for me: have the baby be the player and have to catch the elements he needs
+So either 3 class in total or just 2. 
+
+*/
+
+class Game {
+    constructor(){
+        this.baby=null;
+       }
+
+    start() {
+        this.baby = new Baby;
+
+    }
+
+    moveBaby(direction){
+        if (direction==="right") {
+            this.baby.moveRight();
+        }
+        else if (direction==="left") {
+            this.baby.moveLeft();
+        }
+    }
+}
+
+
 class Baby {
+//baby has to be able to change positions in order to catch the elements he needs
+    constructor(){
+     this.positionX=0;
+    }
+
+    moveRight(){
+     this.positionX++;
+     
+     //console.log just to test the code:
+     console.log(`baby moving to the right ${this.positionX}`)
+    }
+
+    moveLeft(){
+    this.positionX--;
+
+    //console.log just to test the code:
+    console.log(`baby moving to the left ${this.positionX}`)
+    }
+}
+
+
+//obstacle was not the right term for my game: my obstacles are elements that the player must catch
+//each has to be able to change positions
+//I need 4 different necessities for each game start: milk bottle, diaper, nap time, toy
+
+class Necessities {
+    constructor() {
+        this.positionX=0;
+        this.positionY=0;
+        
+    }
+}
+
+class babyBottle extends Necessities {
 
 }
 
-class Nanny {
-//nanny has to be able to change positions
-}
-
-class Obstacle {
+class diaper extends Necessities {
     
 }
+
+class toy extends Necessities {
+    
+}
+
+class napTime extends Necessities {
+    
+}
+//just to test the code:
+//myBaby = new Baby;
+//myBaby.moveRight();
+
+
--- js/dommanip.js
+++ js/dommanip.js
@@ -1,2 +1,25 @@
-console.log("Welcome to the Baby Survival Tips Game")
+console.log("Welcome to the Baby Survival Tips Game");
 
+//Here I put the interaction with the DOM of the HTML
+
+//First I need to start the game: by creating a new instance of it, and applying it the start method
+const game= new Game;
+game.start();
+
+
+//create the dom interaction when you use your keyboard Right & Left
+document.addEventListener("keydown",function(event){
+   //in order to find what is the info displayed when we press a key, we have to console.log the event
+   console.log(event);
+   if (event.key==="ArrowRight") {
+    //need to move the baby right
+    game.moveBaby("right");
+   }
+   else if (event.key==="ArrowLeft") {
+    //need to move the baby left
+    game.moveBaby("left");
+   }
+
+})
+
+//need to create DOM elements: the 4 Necessities: milk bottle, diaper, nap time, toy
